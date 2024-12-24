cb48c2e3a6c23f8adc83091e27985b6ec50a1707
teestojko <158604040+teestojko@users.noreply.github.com>
2024-12-25 01:46:30 +0900

    ”ポートフォリオサンプル作成”

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import LightEffects from "./LightEffects";
+import "./App.css";
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "images/underwater.jpg",
+    "images/sky.jpg",
+  ];
+  const texts = [
+    { comment: "今よりも\nより良い明日へ", credit: "Photo by Cristian Palmer on Unsplash" },
+    { comment: "共に新たな創造を...", credit: "Photo by Sam Schooler on Unsplash" },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // 3秒ごとに画像切り替え
+
+    return () => clearInterval(interval); // クリーンアップ
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="portfolio">
+      <div className="portfolio_inner">
+        <img src={images[currentIndex]} alt="Portfolio" className="active" />
+        <div className="text-overlay active">
+          <div className="portfolio_comment">{texts[currentIndex].comment}</div>
+          <div className="image_credit">{texts[currentIndex].credit}</div>
+        </div>
+
+        {/* 光のエフェクトを追加 */}
+        <LightEffects />
+
+        <div className="custom-background">
+          <div className="overlay-content">
+            <div className="portfolio_company">
+              <p className="company_message hidden">あなたの作りたいを<br />叶えます</p>
+              <p className="company_title hidden">web engineer<br />Tetsuya Kishi</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="extra-content hidden">
+          <svg id="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+            {/* SVGコンテンツをここに追加 */}
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
