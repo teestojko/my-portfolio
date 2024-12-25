f174eebe940659e3e780df3c026fa2549e993b06
teestojko <158604040+teestojko@users.noreply.github.com>
2024-12-26 00:41:41 +0900

    ”画像をトランジション表示”

--- src/utils/changeImage.js
+++ src/utils/changeImage.js
@@ -4,16 +4,14 @@ function useImageChanger(images, texts, interval = 3000) {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        if (!images.length || !texts.length) return;
-
         const changeImage = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
         };
 
         const timer = setInterval(changeImage, interval);
 
-        return () => clearInterval(timer); // コンポーネントがアンマウントされたらクリーンアップ
-    }, [images.length, texts.length, interval]);
+        return () => clearInterval(timer); // クリーンアップ処理
+    }, [images.length, interval]);
 
     return {
         currentImage: images[currentIndex],
