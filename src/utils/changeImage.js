db76741516eb4e34a89bcf21ca1834f7993e4e75
teestojko <158604040+teestojko@users.noreply.github.com>
2024-12-26 00:10:46 +0900

    ”画像をトランジション表示”

--- src/utils/changeImage.js
+++ src/utils/changeImage.js
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
 
 function useImageChanger(images, texts, interval = 3000) {
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    if (!images.length || !texts.length) return;
+    useEffect(() => {
+        if (!images.length || !texts.length) return;
 
-    const changeImage = () => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        const changeImage = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        };
+
+        const timer = setInterval(changeImage, interval);
+
+        return () => clearInterval(timer); // コンポーネントがアンマウントされたらクリーンアップ
+    }, [images.length, texts.length, interval]);
+
+    return {
+        currentImage: images[currentIndex],
+        currentText: texts[currentIndex],
+        currentIndex,
     };
-
-    const timer = setInterval(changeImage, interval);
-
-    return () => clearInterval(timer); // コンポーネントがアンマウントされたらクリーンアップ
-  }, [images.length, texts.length, interval]);
-
-  return {
-    currentImage: images[currentIndex],
-    currentText: texts[currentIndex],
-    currentIndex,
-  };
 }
 
 export default useImageChanger;
