67618d93e5c56dc7471c6fa32767f77a8be446fa
teestojko <158604040+teestojko@users.noreply.github.com>
2024-12-31 01:33:29 +0900

    ”work画面　タイトル表示アニメーション”

--- src/App.js
+++ src/App.js
@@ -106,33 +106,33 @@ function App() {
     };
   }, []);
 
-  useEffect(() => {
-  // const currentRef = workTitleRef.current; // ローカル変数にコピー
+//   useEffect(() => {
+//   const currentRef = workTitleRef.current; // ローカル変数にコピー
 
-  const observer = new IntersectionObserver(
-    ([entry]) => {
-      if (entry.isIntersecting) {
-        currentRef.style.opacity = 1;
-        currentRef.style.transform = "translateX(0)";
-        currentRef.style.transition = "opacity 1s ease-out, transform 1s ease-out";//スライド時間
-      } else {
-        currentRef.style.opacity = 0;
-        currentRef.style.transform = "translateX(500px)";//スライド距離
-      }
-    },
-    { threshold: 0 }
-  );
+//   const observer = new IntersectionObserver(
+//     ([entry]) => {
+//       if (entry.isIntersecting) {
+//         currentRef.style.opacity = 1;
+//         currentRef.style.transform = "translateX(0)";
+//         currentRef.style.transition = "opacity 1s ease-out, transform 1s ease-out";//スライド時間
+//       } else {
+//         currentRef.style.opacity = 0;
+//         currentRef.style.transform = "translateX(500px)";//スライド距離
+//       }
+//     },
+//     { threshold: 0 }
+//   );
 
-  if (currentRef) {
-    observer.observe(currentRef);
-  }
+//   if (currentRef) {
+//     observer.observe(currentRef);
+//   }
 
-  return () => {
-    if (currentRef) {
-      observer.unobserve(currentRef);
-    }
-  };
-}, []);
+//   return () => {
+//     if (currentRef) {
+//       observer.unobserve(currentRef);
+//     }
+//   };
+// }, []);
 
 
 
