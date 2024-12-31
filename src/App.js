f08a76f7d2df73ee38ce5f200aed26d8d3295bae
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-01 01:08:04 +0900

    ”profile画面”

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import "./App.css";
 import Wave from "./Wave";
