24a964bc76339d92cf9dd37156cc990c3b41504a
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-07 22:45:50 +0900

    ”インデント修正”

--- src/profileChangeText.js
+++ src/profileChangeText.js
@@ -1,54 +1,54 @@
 import React, { useState, useEffect, useRef } from "react";
 
 const ProfileChangeText = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  const profileTexts = [
-    "Hi, I'm Tetsuya Kishi, a web engineer specializing in creating interactive and visually stunning web applications.",
-    "My goal is to blend creativity with functionality to deliver excellent user experiences.",
-    "Let's work together to create something amazing!",
-  ];
+    const [isVisible, setIsVisible] = useState(false);
+    const profileTexts = [
+        "Hi, I'm Tetsuya Kishi, a web engineer specializing in creating interactive and visually stunning web applications.",
+        "My goal is to blend creativity with functionality to deliver excellent user experiences.",
+        "Let's work together to create something amazing!",
+    ];
 
-  const textRef = useRef(null);
+    const textRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+        ([entry]) => {
+            if (entry.isIntersecting) {
+            setIsVisible(true);
+            }
+        },
+        { threshold: 0.5 }
+        );
+
+        const currentRef = textRef.current;
+
+        if (currentRef) {
+        observer.observe(currentRef);
         }
-      },
-      { threshold: 0.5 }
+
+        return () => {
+        if (currentRef) {
+            observer.unobserve(currentRef);
+        }
+        };
+    }, []);
+
+    return (
+        <p ref={textRef} className="profile-detail">
+        {profileTexts.map((text, index) => (
+            <span
+            key={index}
+            className={`fade-in-text ${isVisible ? "visible" : ""}`}
+            style={{
+                display: "block",
+                transitionDelay: `${index * 3}s`, // 各行に2秒ずつ遅らせてフェードイン
+            }}
+            >
+            {text}
+            </span>
+        ))}
+        </p>
     );
-
-    const currentRef = textRef.current;
-
-    if (currentRef) {
-      observer.observe(currentRef);
-    }
-
-    return () => {
-      if (currentRef) {
-        observer.unobserve(currentRef);
-      }
-    };
-  }, []);
-
-  return (
-    <p ref={textRef} className="profile-detail">
-      {profileTexts.map((text, index) => (
-        <span
-          key={index}
-          className={`fade-in-text ${isVisible ? "visible" : ""}`}
-          style={{
-            display: "block",
-            transitionDelay: `${index * 3}s`, // 各行に2秒ずつ遅らせてフェードイン
-          }}
-        >
-          {text}
-        </span>
-      ))}
-    </p>
-  );
 };
 
 export default ProfileChangeText;
