5036fa54f75736993b041338b6e06dec5065044f
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-07 23:39:33 +0900

    ”画像クリック遷移”

---
+++ src/FurimaPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FurimaPage() {
+  return (
+    <div>
+      <h1>Furima Project</h1>
+      <p>Furimaに関する詳細情報です。</p>
+    </div>
+  );
+}
+
+export default FurimaPage;
---
+++ src/ResePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ResePage() {
+  return (
+    <div>
+      <h1>Rese Project</h1>
+      <p>Reseに関する詳細情報です。</p>
+    </div>
+  );
+}
+
+export default ResePage;
