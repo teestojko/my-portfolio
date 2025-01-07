84c1aee6e0c43ad300cfb208cce774f6aea58ddc
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-08 00:02:11 +0900

    ”画像クリック遷移”

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import { useTextAnimation } from "./changeText";
 import MovingCube from "./movingCube";
