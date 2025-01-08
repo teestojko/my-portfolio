2069d38ddaae0fb96ebdd7dd61cedeb754aee3ee
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-08 16:20:03 +0900

    ”飲食店ページ作成”

--- src/ResePage.js
+++ src/ResePage.js
@@ -3,9 +3,51 @@ import "./rese-page.css";
 
 function ResePage() {
   return (
-    <div className="rese-img-all">
-      <img className="rese-img" src="/images/shop-detail.png" alt="shop-detail" />
-      <img className="rese-img" src="/images/my-page.png" alt="my-page" />
+    <div className="rese">
+      <div className="rese-img-all">
+        <img className="rese-img" src="/images/shop-detail.png" alt="shop-detail" />
+        <img className="rese-img" src="/images/my-page.png" alt="my-page" />
+      </div>
+      <div className="rese-text-all">
+        <ul>
+          <li className="rese-text">
+            fortifyのメール認証を使用したログイン機能
+          </li>
+          <li className="rese-text">
+            
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+          <li className="rese-text">
+
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
