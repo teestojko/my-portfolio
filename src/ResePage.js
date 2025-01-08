afca78c68dc3ff10968eb98771c0395ee1577b91
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-08 13:07:30 +0900

    ”飲食店ページ作成”

--- src/ResePage.js
+++ src/ResePage.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function ResePage() {
   return (
-    <div>
-      <h1>Rese Project</h1>
-      <p>Reseに関する詳細情報です。</p>
+    <div className="rese-img">
+      <img className="shop-detail-img" src="/images/shop-detail.png" alt="shop-detail" />
+      <img className="my-page-img" src="/images/my-page.png" alt="my-page" />
     </div>
   );
 }
