62c89b60cad93b1486b6673772b7ca06bcdcb709
teestojko <158604040+teestojko@users.noreply.github.com>
2025-01-10 00:22:35 +0900

    ”work背景”

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ function App() {
 
           <div className="svg-container">
             <svg
-              viewBox="0 0 100 100"
+              viewBox="0 0 100 150"
               xmlns="http://www.w3.org/2000/svg"
               width="100%"
               height="100%"
@@ -110,29 +110,43 @@ function App() {
                   <stop offset="100%" style={{ stopColor: "rgb(209, 135, 83)", stopOpacity: 1 }} />
                 </radialGradient>
 
-                {/* ぼやけ効果のフィルター */}
-                <filter id="blurFilter" x="0" y="0" width="200%" height="200%">
-                  <feGaussianBlur in="SourceGraphic" stdDeviation="0" />
-                </filter>
+                {/* 茎用グラデーション */}
+                <linearGradient id="stemGrad" x1="0%" y1="0%" x2="0%" y2="100%">
+                  <stop
+                    offset="0%"
+                    style={{ stopColor: "rgb(0,128,0)", stopOpacity: 1 }}
+                  />
+                  <stop
+                    offset="100%"
+                    style={{ stopColor: "rgb(34,139,34)", stopOpacity: 1 }}
+                  />
+                </linearGradient>
               </defs>
 
-              {/* 全体にぼやけ効果を適用 */}
-              <g filter="url(#blurFilter)">
-                {/* 花びら */}
-                <g transform="translate(50,50)">
-                  {Array.from({ length: 24 }).map((_, i) => (
-                    <path
-                      key={i}
-                      d="M0,-30 Q5,-15 0,0 Q-5,-15 0,-30"
-                      fill="url(#petalGrad)"
-                      transform={`rotate(${i * 15})`}
-                    />
-                  ))}
-                </g>
+              {/* 茎 */}
+              <rect
+                x="48"
+                y="60"
+                width="4"
+                height="70"
+                fill="url(#stemGrad)"
+                rx="2"
+              />
+
+              {/* 花びら */}
+              <g transform="translate(50,50)">
+                {Array.from({ length: 24 }).map((_, i) => (
+                  <path
+                    key={i}
+                    d="M0,-30 Q5,-15 0,0 Q-5,-15 0,-30"
+                    fill="url(#petalGrad)"
+                    transform={`rotate(${i * 15})`}
+                  />
+                ))}
+              </g>
 
                 {/* 種の部分 */}
                 <circle cx="50" cy="50" r="10" fill="url(#seedGrad)" />
-              </g>
             </svg>
           </div>
 
