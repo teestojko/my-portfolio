6ac0837f96f46aadc725a752ef2b557687c9f6fc
teestojko <158604040+teestojko@users.noreply.github.com>
2025-03-24 21:09:52 +0900

    ”動きのスムーズさ修正中”

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef,  } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
 import { useTextAnimation } from "./changeText";
