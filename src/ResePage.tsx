093cadebe28fad11e00440bfbbcf2dffaac6fb6d
teestojko <158604040+teestojko@users.noreply.github.com>
2025-03-30 15:48:07 +0900

    ”rese画像表示修正”

--- src/ResePage.tsx
+++ src/ResePage.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
 import "./rese-page.css";
 
 const ResePage: React.FC = () => {
@@ -13,10 +18,27 @@ const ResePage: React.FC = () => {
   return (
     <div className="rese">
       <div className="rese-title">飲食店予約アプリ</div>
-      <div className="rese-img-all">
+      {/* <div className="rese-img-all">
         <img className="rese-img" src="/images/shop-detail.png" alt="Shop Detail" />
         <img className="rese-img" src="/images/rese-my-page.png" alt="My Page" />
-      </div>
+      </div> */}
+      {/* Swiperで横スクロール */}
+            <div className="furima-img-all">
+              <Swiper
+                spaceBetween={10}
+                slidesPerView={1}  // 一度に表示するスライドを1つに固定
+                navigation={true}  // クリックでスライド
+                grabCursor={false} // カーソル変更を無効化
+                modules={[Navigation]} // 遅延読み込みを追加
+              >
+                <SwiperSlide>
+                  <img className="rese-img" src="/images/shop-detail.png" alt="Shop Detail" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="rese-img" src="/images/rese-my-page.png" alt="My Page" />
+                </SwiperSlide>
+              </Swiper>
+            </div>
       <div className="rese-text-all">
         <ul className="rese-ul">
           <li className="rese-text-page">飲食店一覧、詳細、レビューページ &マイページ</li>
