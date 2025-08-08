9f39d66cc9dc446440e7144d3abd31c9a3af6392
Tetsuya Kishi <158604040+teestojko@users.noreply.github.com>
2025-08-08 20:03:01 +0900

    問い合わせデザイン修正

--- src/App.tsx
+++ src/App.tsx
@@ -150,8 +150,9 @@ const App: React.FC = () => {
       <div className="contact">
         <div ref={contactRef} className="section contact-section">
           <h1 className="contact-title">Contact</h1>
-          {/* <p className="contact-mail">Email: **************@gmail.com</p> */}
-          Email: <a className="contact-mail" href="mailto:7195.teesu.1031@gmail.com">7195.teesu.1031@gmail.com</a>
+          <div>
+            Email: <a className="contact-mail" href="mailto:7195.teesu.1031@gmail.com">7195.teesu.1031@gmail.com</a>
+          </div>
         </div>
       </div>
     </div>
